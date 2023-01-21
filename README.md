fe4d3c38831dec3adce84c9ddbfabb82cd0feeca
ABo-EsMaiL <123252905+ABo-EsMaiL@users.noreply.github.com>
2023-01-21 16:38:15 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# udacity-landng-page
+# Landing Page Project
+
+It is the first project that I have completed and I hope that I have implemented what is required.
+
+I created a function to create a link for each section by using for of loop.
+
+add the active class properties for both section.
+
+# reference
+
+- For...of - JavaScript | MDN => http://bit.ly/3ZT8jRd
+-Element.getBoundingClientRect() - Web APIs | MDN =>http://bit.ly/3wDjzUN
---
+++ js/app.js
@@ -0,0 +1,87 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const sections = Array.from(document.querySelectorAll("section"));
+const navbar = document.getElementById("navbar__list");
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+function createList() {
+  for (section of sections) {
+    sectionName = section.getAttribute("data-nav");
+    sectionLink = section.getAttribute("id");
+    // create item for each one
+    listCreate = document.createElement("li");
+    // add the item text
+    listCreate.innerHTML = `<a href="#${sectionLink}" data-nav="${sectionLink}" class="menu__link">${sectionName}</a>`;
+    // add listCreate to menu
+    navbar.appendChild(listCreate);
+  }
+}
+
+// Add class 'active' to section when near top of viewport
+window.onscroll = function () {
+  document.querySelectorAll("section").forEach(function (active) {
+    if (
+      active.getBoundingClientRect().top >= -300 &&
+      active.getBoundingClientRect().top <= 400
+    ) {
+      active.classList.add("your-active-class");
+    } else {
+      active.classList.remove("your-active-class");
+    }
+  });
+};
+
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+createList();
+// Scroll to section on link click
+navbar.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.dataset.nav) {
+    document
+      .getElementById(`${event.target.dataset.nav}`)
+      .scrollIntoView({ behavior: "smooth" });
+  }
+});
+// Set sections as active
